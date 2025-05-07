7a94a9d7d55497ec659a9229533cdfdede37b549
Victor <victorgab0802@gmail.com>
2025-05-07 09:10:34 -0300

    reformulando desing do login

--- front_end/src/pages/login/login.js
+++ front_end/src/pages/login/login.js
@@ -57,10 +57,10 @@ const Login = () => {
   }
 
     return (
-      <div className="App w-screen h-screen bg-fundo flex items-center justify-center">
-        <div className = "flex w-auto h-5/6 items-center justify-center font-poppins font-medium bg-white">
+      <div className="App w-screen h-screen bg-zinc-800 flex items-center justify-center">
+        <div className = "flex w-auto h-5/6 items-center justify-center font-poppins font-medium rounded-3xl box bg-zinc-900">
 
-            {vis && (<form className="flex flex-col items-center justify-center w-4/6 h-full gradient" onSubmit={handleSubmit}>
+            {vis && (<form className="flex flex-col items-center justify-center w-4/6 h-full " onSubmit={handleSubmit}>
                 <h2 className="text-white text-4xl font-semibold">Login</h2>
 
                 <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-white hover:opacity-70" type="text" placeholder="nome"></input>
@@ -74,18 +74,18 @@ const Login = () => {
               </p>
             </form>)}
 
-            <div className={`flex items-center justify-center flex-col w-4/6 h-full bg-white mover ${anim ? 'troca' : ''}`} >
+            <div className={`flex items-center justify-center flex-col w-4/6 h-full bg-zinc-900 rounded-3xl  mover ${vis2 ? 'rounded-l-3xl' : 'rounded-r-3xl'} ${anim ? 'troca' : ''}`} >
               <h1 className="text-red-600 text-6xl font-semibold mudar"> Fake analisys </h1>
-              <p className= "text-center text-zinc-800 mt-3"> site desenvolvido para ajudar pessoas a descobrir se uma notícia é verídica ou não  </p>
+              <p className= "text-center text-white mr-[0.7em] mt-3"> site desenvolvido para ajudar pessoas a descobrir se uma notícia é verídica ou não  </p>
             </div>
 
              {vis2 &&( <form className="flex flex-col items-center justify-center w-4/6 h-full gradient" onSubmit={criar_usu}>
 
                 <h2 className="text-white text-4xl">Cadastre-se</h2>
                 
-                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:bg-white hover:border-none hover:p-1.5" type="text" placeholder="nome"></input>
-                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:bg-white hover:border-none hover:p-1.5" type="email" placeholder="Email"></input>
-                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:bg-white hover:border-none hover:p-1.5" type="password" placeholder="Senha"></input>
+                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:opacity-70" type="text" placeholder="nome"></input>
+                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:opacity-70" type="email" placeholder="Email"></input>
+                <input className="flex p-1 mt-2 rounded-lg border-2 border-solid decoration-none outline-none bg-transparent text-zinc-200 hover:opacity-70" type="password" placeholder="Senha"></input>
                 
                 <button className= "flex items-center justify-center p-2 bg-transparent text-white border border-solid border-white mt-1 w-32 rounded-lg cursor-pointer hover:opacity-70" type="submite">cadastrar</button>
                 
