5c0db2a1540441a2fb3bfe549ebde06d8be77d7a
Victor <victorgab0802@gmail.com>
2025-05-09 16:46:04 -0300

    testando APIs

--- front_end/src/pages/home/home.js
+++ front_end/src/pages/home/home.js
@@ -1,9 +1,25 @@
-import { GoArrowRight } from "react-icons/go";
 import { HiArrowCircleUp } from "react-icons/hi";
-import { FaRegUser } from "react-icons/fa";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [noticia, setnoticia] = useState([])
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.rss2json.com/v1/api.json?rss_url=https://g1.globo.com/rss/g1/`
+        );
+        console.log(response.data.items);
+      } catch (error) {
+        console.error('Erro ao buscar notícias:', error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
 
  return (
    <div className= "flex flex-col  h-screen ">
@@ -17,7 +33,7 @@ const Home = () => {
                           <p className ="text-white ">Qual a analise de hoje?</p>
                       </div>
                       <div className = "bg-zinc-700 rounded-xl flex flex-row justify-between items-center w-3/4 h-1/6 bs">
-                        <input className= "w-5/6 h-full outline-none bg-transparent border-none text-white pl-2 m-1 text-base" placeholder="Digite o título da notícia aqui" type="text"></input>
+                        <input className= "w-5/6 h-full outline-none bg-transparent border-none text-white pl-3 text-base" placeholder="Digite o título da notícia aqui" type="text"></input>
                         <div className = "w-1/6 h-full flex items-center justify-center ">
                           <HiArrowCircleUp className= "cursor-pointer text-5xl text-zinc-500" />
                          </div>
